f04c84f1b77b40819ca47fe2a6464e2f65edefdc
iPul_noPi <syaifulscooter@gmail.com>
2019-11-15 15:24:42 +0700

    tes mengeluarkan /  menggunakan faker

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,4 +11,38 @@ class Welcome extends CI_Controller {
 								);
 		$this->load->view('list_mhs', $kata2);
 	}
+
+
+public function ContohFaker()
+	{
+		include APPPATH.'/third_party/faker/autoload.php';
+
+		$faker = Faker\Factory::create();
+		$faker->seed(2);
+
+		echo "<strong>Title: </strong>".$faker->title;
+		echo "<br>";
+
+		echo "<strong>First Name: </strong>".$faker->firstName;
+		echo "<br>";
+
+		echo "<strong>Last Name: </strong>".$faker->lastName;
+		echo "<br>";
+
+		echo "<strong>Street Address: </strong>".$faker->streetAddress;
+		echo "<br>";
+
+		echo "<strong>Postcode: </strong>".$faker->postcode;
+		echo "<br>";
+
+		echo "<strong>City: </strong>".$faker->city;
+		echo "<br>";
+
+		echo "<strong>Country: </strong>".$faker->country;
+		echo "<br>";
+
+
+		//$this->load->view('welcome_message');
+	}
+
 }
